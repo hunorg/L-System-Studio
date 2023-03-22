2377131a39cead92bbd87eaae22c00aef1a06d83
hunorgered <114682020+hunorg@users.noreply.github.com>
2023-03-23 00:18:39 +0100

    Update Model.elm

--- src/Model.elm
+++ src/Model.elm
@@ -1,6 +1,8 @@
 module Model exposing (Model, Symbol, init)
 
 import Turtle exposing (Action(..))
+import Color
+import ColorPicker
 
 
 type alias Model =
@@ -10,13 +12,17 @@ type alias Model =
     , selectedSymbol : String
     , selectedAction : Action
     , newRuleInput : String
-    , angle : Float
+    , turningAngle : Float
+    , turningAngleIncrement : Float
     , lineLength : Float
+    , lineLengthScale : Float
     , lineWidthIncrement : Float
     , axiom : String
     , iterations : Int
     , startingPoint : ( Float, Float )
     , startingAngle : Float
+    , polygonFillColor : Color.Color
+    , colorPicker : ColorPicker.State
     , generatedSequence : List Char
     , drawnTurtle : Bool
     }
@@ -45,27 +51,41 @@ init =
         , { character = "]", action = Pop }
         , { character = "#", action = IncrementLineWidth }
         , { character = "@", action = DrawDot }
-        ] ++ emptySymbolAssignments
+        , { character = "{", action = OpenPolygon }
+        , { character = "}", action = ClosePolygon }
+        , { character = "<", action = MultiplyLength }
+        , { character = ">", action = DivideLength }
+        , { character = "&", action = SwapPlusMinus }
+        , { character = "(", action = DecrementTurningAngle }
+        , { character = ")", action = IncrementTurningAngle }
+        ]
+            ++ emptySymbolAssignments
     , syntaxDisplay = False
     , rules = []
     , selectedSymbol = ""
     , selectedAction = Move
     , newRuleInput = ""
-    , angle = 0
+    , turningAngle = 0
+    , turningAngleIncrement = 0
     , lineLength = 1
+    , lineLengthScale = 0
     , lineWidthIncrement = 0
     , axiom = ""
     , iterations = 0
     , startingPoint = ( 700, 500 )
     , startingAngle = 0
+    , polygonFillColor = Color.red
+    , colorPicker = ColorPicker.empty
     , generatedSequence = []
     , drawnTurtle = False
     }
 
+
 emptySymbolAssignments : List Symbol
 emptySymbolAssignments =
     let
-        lowercaseChars = ['a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+        lowercaseChars =
+            [ 'a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
         createSymbol char =
             { character = String.fromChar char, action = NoAction }
